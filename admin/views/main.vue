060b6a172b39b4402f9f4db4505af72d93719ef3
mrmnmly <mrmnmly@gmail.com>
2017-10-12 00:21:11 +0200

    Trying to import 3rd party component inside vue single file component.

--- admin/views/main.vue
+++ admin/views/main.vue
@@ -2,15 +2,21 @@
   <div>
     <h1>Hello world!</h1>
     <p>Welcome to {{title}}</p>
+    <app-sidebar></app-sidebar>
   </div>
 </template>
 
 <script>
+  import Sidebar from './sidebar.vue';
+
   export default {
     data() {
       return {
         title: 'Admin panel'
       }
+    },
+    components: {
+      appSidebar: Sidebar
     }
   }
 </script>
---
+++ admin/views/sidebar.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    {{ lists }}
+  </div>
+</template>
+
+<script>
+  import axios from 'axios';
+
+  export default {
+    data() {
+      return {
+        lists: {}
+      }
+    },
+    created() {
+      this.getSidebarData();
+    },
+    methods: {
+      getSidebarData() {
+        axios.get('http://locahost:3000/list-contents/').then(data => {
+          this.lists = data;
+        });
+      }
+    }
+  }
+</script>
+
+<style lang="css">
+
+</style>
