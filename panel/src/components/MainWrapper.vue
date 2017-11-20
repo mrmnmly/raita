6fb416108d51e77afc95ce6265be043ef024d8dc
mrmnmly <mrmnmly@gmail.com>
2017-11-20 14:48:23 +0100

    Created main app wrapper component.

---
+++ panel/src/components/MainWrapper.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="main-wrapper">
+    <sidebar />
+  </div>
+</template>
+
+<script>
+import Sidebar from './Sidebar';
+
+export default {
+  components: {
+    'sidebar': Sidebar,
+  },
+};
+</script>
+
+<style lang="scss">
+.main-wrapper {
+  background: $gray-lighter;
+  bottom: 0;
+  display: block;
+  left: 0;
+  margin: 0;
+  padding: 0;
+  position: fixed;
+  right: 0;
+  top: 0;
+  width: 100%;
+}
+</style>
