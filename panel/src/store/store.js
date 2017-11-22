a693e9df50bf0af071a6bed2fe204c8249966d51
mrmnmly <mrmnmly@gmail.com>
2017-11-22 14:19:12 +0100

    Added new store modules to main store.

--- panel/src/store/store.js
+++ panel/src/store/store.js
@@ -1,12 +1,16 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 
-import lists from './modules/lists'
+import sidebar from './modules/sidebar';
+import lists from './modules/lists';
+import article from './modules/article';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   modules: {
-    lists
+    sidebar,
+    lists,
+    article,
   },
 });
