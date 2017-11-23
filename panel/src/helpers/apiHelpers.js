7f8faa3c82cdc65b45c2d46d22c231768e1e6558
mrmnmly <mrmnmly@gmail.com>
2017-11-23 23:53:30 +0100

    Added api helper method for saving files.

--- panel/src/helpers/apiHelpers.js
+++ panel/src/helpers/apiHelpers.js
@@ -10,13 +10,30 @@ export function getApiLists() {
   });
 };
 
-// article object has to contain folder and file (name + extension, .md mostly) property
+// article object has to contain folder name (text) and filename (text, name + extension, .md mostly) property
 export function getApiArticleContents(article) {
   return fetch(`${config.api.domain}${config.api.endpoints.getArticleContents}${article.folder}/${article.file}`).then(resp => {
     if (resp.ok) {
       return resp.json();
     }
     console.error('There was an error during fetching article contents.');
+    return {};
+  });
+};
+
+// article object has to contain content (text), url (text) and customFields property (object)
+export function saveApiArticle(articleObj) {
+  return fetch(`${config.api.domain}${config.api.endpoints.saveArticle}`, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      method: 'POST',
+      body: JSON.stringify(articleObj),
+    }).then(resp => {
+    if (resp.ok) {
+      return resp.json();
+    }
+    console.error('There was an error during saving file.');
     return {};
   });
 };
