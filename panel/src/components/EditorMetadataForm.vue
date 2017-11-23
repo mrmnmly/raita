d28ee9f50adaf5a7a9dbe747e79986e60d927712
mrmnmly <mrmnmly@gmail.com>
2017-11-23 23:52:35 +0100

    WIP on metadata component.

--- panel/src/components/EditorMetadataForm.vue
+++ panel/src/components/EditorMetadataForm.vue
@@ -1,7 +1,18 @@
 <template>
-  <div>
-    {{ metadata }}
-  </div>
+  <ul class="metadata-form">
+    <li
+      class="metadata-form__entry"
+      v-for="(item, key, index) in metadata"
+      :key="`${key}-${index}`"
+    >
+      <input
+        class="metadata-form__input"
+        type="text"
+        :placeholder="key"
+        :value="item"
+      />
+    </li>
+  </ul>
 </template>
 
 <script>
@@ -21,5 +32,31 @@ export default {
 </script>
 
 <style lang="scss">
+.metadata-form {
+  display: block;
+  list-style-type: none;
+  margin: $regular-margin auto;
+  padding: 0;
+  width: $editor-width;
 
+  .metadata-form__entry {
+    display: block;
+    width: 100%;
+  }
+
+  .metadata-form__input {
+    @include input-border($white-darker);
+
+    background: transparent;
+    margin: $regular-margin 0;
+    padding: $small-padding $regular-padding;
+    transition: border 0.5s;
+
+    &:focus {
+      @include input-border($red);
+
+      transition: border 0.5s;
+    }
+  }
+}
 </style>
