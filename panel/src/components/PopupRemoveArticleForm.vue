d427bd6c18227d9319e82a90d8fbdcb3f66c7624
mrmnmly <mrmnmly@gmail.com>
2017-12-07 15:08:06 +0100

    Added popup for confirmation of article removal.

---
+++ panel/src/components/PopupRemoveArticleForm.vue
@@ -0,0 +1,67 @@
+<template>
+  <div>
+    <p class="popup__question">Are you sure to remove {{ articleToRemove.slug }}?</p>
+    <button
+      class="popup__button"
+      @click.prevent="removeArticle"
+    >
+      Yes
+    </button>
+    <button
+      class="popup__button"
+      @click.prevent="dismissPopup"
+    >
+      No
+    </button>
+  </div>
+</template>
+
+<script>
+import updateSidebarDataMixin from './../mixins/updateSidebarDataMixin';
+import { removeApiFile } from './../helpers/apiHelpers';
+
+export default {
+  mixins: [ updateSidebarDataMixin ],
+  computed: {
+    articleToRemove() {
+      return this.$store.getters.getArticleToRemove;
+    },
+    selectedArticle() {
+      return this.$store.getters.getSelectedArticle;
+    }
+  },
+  methods: {
+    dismissPopup() {
+      this.$store.dispatch('updateArticleToRemove', {});
+      this.$store.dispatch('closePopup', 'removePopup');
+    },
+    removeArticle() {
+      removeApiFile(this.articleToRemove.path).then(() => {
+        let articleToSelect = {};
+        if (this.selectedArticle.path !== this.articleToRemove.path) {
+          articleToSelect = this.selectedArticle;
+        }
+        // method from imported updateSidebarDataMixin
+        this.updateSidebarData(articleToSelect).then(() => {
+          this.dismissPopup();
+        });
+      });
+    },
+  }
+};
+</script>
+
+<style lang="scss">
+.popup__question {
+  margin-bottom: $big-margin;
+  padding: 0 $regular-padding;
+}
+
+.popup__button {
+  @include black-button($black, $white, $big-padding, $regular-padding);
+
+  &:hover {
+    @include black-button-hover($red);
+  }
+}
+</style>
