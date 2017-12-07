974120c1477427d80b4005c365c51150f9eff120
mrmnmly <mrmnmly@gmail.com>
2017-12-07 23:58:26 +0100

    Added page vuex store.

---
+++ panel/src/store/modules/pages.js
@@ -0,0 +1,41 @@
+const state = {
+  pages: {},
+  selectedPage: null,
+};
+
+const getters = {
+  getPages(state) {
+    return state.pages;
+  },
+  getSelectedPage(state) {
+    return state.selectedPage;
+  }
+};
+
+const mutations = {
+  updatePages(state, payload) {
+    state.pages = payload;
+  },
+  selectPage(state, payload) {
+    state.selectedPage = payload;
+  },
+};
+
+const actions = {
+  updatePages({ commit }, payload) {
+    return new Promise((resolve) => {
+      commit('updatePages', payload);
+      resolve();
+    });
+  },
+  selectPage({ commit }, payload) {
+    commit('selectPage', payload);
+  },
+};
+
+export default {
+  state,
+  mutations,
+  actions,
+  getters,
+};
