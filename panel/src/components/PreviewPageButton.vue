f52f67af2b3c4170ef2716b3fa96db99ada99783
mrmnmly <mrmnmly@gmail.com>
2017-12-11 23:24:55 +0100

    Added button to compile and preview created website.

---
+++ panel/src/components/PreviewPageButton.vue
@@ -0,0 +1,38 @@
+<template>
+  <button
+    class="sidebar__preview-button"
+    title="Preview compiled page"
+    @click.prevent="compileAndPreview"
+  >
+    Preview compiled page
+  </button>
+</template>
+
+<script>
+import { compileApiContents } from './../helpers/apiHelpers';
+export default {
+  methods: {
+    compileAndPreview() {
+      compileApiContents().then(() => {
+        document.location.href = '/public';
+      });
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.sidebar__preview-button {
+  @include button($gray, $white, $big-padding, $regular-padding);
+
+  display: block;
+  margin: $regular-margin auto;
+
+  &:hover {
+    @include button-hover($red);
+
+    color: $white;
+  }
+}
+
+</style>
