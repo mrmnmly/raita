684b537cfd0f0744541ee78ea9499eaf588a804d
mrmnmly <mrmnmly@gmail.com>
2017-12-20 00:13:09 +0100

    Updated mrmnmly theme.

--- api/theme/mrmnmly/static/js/main.js
+++ api/theme/mrmnmly/static/js/main.js
@@ -1,11 +1,31 @@
-const servicesData = fetch('/static/assets/json/services.json');
+const app = {
+  getCurrentYear() {
+    const date = new Date();
+    return date.getFullYear();
+  },
 
-(function(){
-  Promise.all([servicesData]).then(servicesData => {
-    servicesData[0].json().then(data => {
-      console.log(data);
-    });
-  });
-}());
+  setCurrentYearInFooter() {
+    const currentYear = this.getCurrentYear();
+    const yearDomWrapper = document.getElementById('fotter__year');
+    if (yearDomWrapper) {
+      yearDomWrapper.textContent = currentYear;
+    }
+  },
 
-alert('blaa');
+  setYearsOfExperience () {
+    const startingYear = 2011;
+    const currentYear = this.getCurrentYear();
+    const yearsOfExperience = currentYear - startingYear;
+    const yearsOfExperienceDomWrapper = document.getElementById('experience__years-counter');
+    if (yearsOfExperienceDomWrapper) {
+      yearsOfExperienceDomWrapper.textContent = yearsOfExperience;
+    }
+  },
+
+  init() {
+    this.setCurrentYearInFooter();
+    this.setYearsOfExperience();
+  },
+};
+
+app.init();
